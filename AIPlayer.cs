4e472004ca4238ccfb60509f68841a37cc20fc4a
Avichai <avichai504@gmail.com>
2024-02-17 14:09:42 +0200

    Local commit

--- AIPlayer.cs
+++ AIPlayer.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace A24_Ex02_NeomiBashari_207087487_AvichaiGalOr_207051848
+﻿namespace A24_Ex02_NeomiBashari_207087487_AvichaiGalOr_207051848
 {
-    internal class AIPlayer
+    public class AIPlayer : Player
     {
+        // AI-specific attributes
+
+        public override void MakeMove(Board board)
+        {
+            // Override with AI logic for making a move
+        }
     }
 }
